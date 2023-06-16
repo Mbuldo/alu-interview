a63dbe355de5f5622cd7eddeb296b07d1978803d
Mbuldo <m.buldo@alustudent.com>
2023-06-16 08:27:24 +0000

    task done

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
-   Python script that calculates how much water is retained after it rains on the walls
+    Python script that calculates how much water is retained after it rains
 """
 
 
 def rain(walls):
     """
-       Function that calculates how much water will be retainedd after it rains
-       Return:
-           int: amount of water collectec
+        Function that calculates how much water will be retained after it rains
+        Return:
+            int: amount of rainwater collected
     """
     if walls is None:
         return 0
@@ -22,9 +22,9 @@ def rain(walls):
     for i in range(1, walls_length - 1):
         left = walls[i]
         for j in range(i):
-            left = max(lefft, walls[j])
+            left = max(left, walls[j])
         right = walls[i]
-        for j in range(i + 1, walls_length)
+        for j in range(i + 1, walls_length):
             right = max(right, walls[j])
-        max_water = max_water + (min(left, right) - walls{i])
+        max_water = max_water + (min(left, right) - walls[i])
     return max_water
